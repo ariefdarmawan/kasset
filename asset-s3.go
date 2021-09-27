cef9be65421545112972481dda834ac27c9b4fc4
Arief Darmawan <ariefda@hotmail.com>
2021-09-27 14:37:58 +0700

    add s3 driver

--- asset-s3.go
+++ asset-s3.go
@@ -1,25 +1,107 @@
 package kasset
 
+import (
+	"bytes"
+	"io"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
 type S3Asset struct {
 	key, secret, bucket string
+	svc                 *s3.S3
 }
 
-func NewS3(key, secret, bucket string) (*S3Asset, error) {
-	s3 := new(S3Asset)
-	s3.key = key
-	s3.secret = secret
-	s3.bucket = bucket
-	return s3, nil
+func Newfs(key, secret, bucket string) (*S3Asset, error) {
+	fs := new(S3Asset)
+	fs.key = key
+	fs.secret = secret
+	fs.bucket = bucket
+
+	sess, e := session.NewSession()
+	if e != nil {
+		return nil, e
+	}
+
+	s3svc := s3.New(sess)
+	listInput := &s3.ListObjectsInput{Bucket: aws.String(bucket), MaxKeys: aws.Int64(16)}
+	if _, e := s3svc.ListObjects(listInput); e != nil {
+		if awsErr, ok := e.(awserr.Error); ok {
+			switch awsErr.Code() {
+			case s3.ErrCodeNoSuchBucket:
+				createInput := &s3.CreateBucketInput{
+					Bucket: aws.String(bucket),
+				}
+				if _, e = s3svc.CreateBucket(createInput); e != nil {
+					return nil, e
+				}
+			default:
+				return nil, e
+			}
+		} else {
+			return nil, e
+		}
+	}
+
+	fs.svc = s3svc
+	return fs, nil
+}
+
+func (sfs *S3Asset) getService() *s3.S3 {
+	if sfs.svc == nil {
+		sess, _ := session.NewSession()
+		sfs.svc = s3.New(sess)
+	}
+	return sfs.svc
 }
 
 func (sfs *S3Asset) Save(name string, bs []byte) error {
+	byteReader := bytes.NewReader(bs)
+
+	svc := sfs.getService()
+	putInput := s3.PutObjectInput{
+		Bucket: aws.String(sfs.bucket),
+		Body:   aws.ReadSeekCloser(byteReader),
+		Key:    aws.String(name),
+	}
+	_, err := svc.PutObject(&putInput)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (sfs *S3Asset) Read(name string) ([]byte, error) {
-	return []byte{}, e
+	bs := []byte{}
+	buffer := bytes.NewBuffer(bs)
+
+	svc := sfs.getService()
+	readParam := s3.GetObjectInput{
+		Bucket: aws.String(sfs.bucket),
+		Key:    aws.String(name),
+	}
+	readResult, err := svc.GetObject(&readParam)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer readResult.Body.Close()
+	if _, err := io.Copy(buffer, readResult.Body); err != nil {
+		return []byte{}, err
+	}
+	return buffer.Bytes(), nil
 }
 
 func (sfs *S3Asset) Delete(name string) error {
+	svc := sfs.getService()
+	delInput := s3.DeleteObjectInput{
+		Bucket: aws.String(sfs.bucket),
+		Key:    aws.String(name),
+	}
+	if _, err := svc.DeleteObject(&delInput); err != nil {
+		return err
+	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -8,11 +8,11 @@ require (
 	git.kanosolution.net/kano/kaos v0.1.1
 	github.com/ariefdarmawan/datahub v0.2.0
 	github.com/ariefdarmawan/flexmgo v0.2.2
+	github.com/aws/aws-sdk-go v1.40.49
 	github.com/eaciit/toolkit v0.0.0-20210610161449-593d5fadf78e
 	github.com/google/uuid v1.3.0
 	github.com/h2non/filetype v1.1.1
 	github.com/kanoteknologi/hd v0.0.5
 	github.com/kanoteknologi/khc v0.0.0-20201106040214-bfaf5bb62d14
 	github.com/smartystreets/goconvey v1.6.4
-	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 )
